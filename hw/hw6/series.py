bd4b88ddaec7323e681ae5d460a4b58f39acfd22
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-06-18 15:36:23 -0700

    Created file, wrote if/else

---
+++ hw/hw6/series.py
@@ -0,0 +1,9 @@
+"""Function returns the nth value of the fibonacci series"""
+def fibonacci(n):
+
+    if (n == 0):
+        return 0
+    elif  (n == 1):
+        return 1
+    elif (n > 1):
+        return
