fffce94ce4805124927df532162a31cd3f7b01ca
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-06-21 07:04:58 -0400

    Added lucas numbers

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -1,9 +1,26 @@
-"""Function returns the nth value of the fibonacci series"""
 def fibonacci(n):
+ """Returns the nth value of the Fibonacci series"""
 
     if (n == 0):
         return 0
     elif  (n == 1):
         return 1
     elif (n > 1):
-        return
+         return (fibonacci(n -1) + fibonacci(n-2))
+
+print(fibonacci(8))
+
+
+
+def lucas(n):
+    """Returns the nth value of the Lucas series"""
+
+    if (n ==0):
+        return(2)
+    elif (n == 1):
+        return(1)
+    elif (n > 1):
+        return(lucas(n - 1) + lucas(n - 2))
+
+print(lucas(3))
+
