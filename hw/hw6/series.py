f064423e407a1204bc41a79430cf39ec41138e51
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-06-22 18:24:43 -0700

    Finished sum_series to calc both fibonacci and lucas numbers

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -7,7 +7,7 @@ def fibonacci(n):
     elif (n > 1):
          return (fibonacci(n -1) + fibonacci(n-2))
 
-print(fibonacci(4))
+print(fibonacci(3))
 
 
 
@@ -27,12 +27,15 @@ print(lucas(3))
 
 
 def sum_series(n, x=0, y=1):
-    if (n == 0):
-        return 0
-    elif  (n == 1):
-        return 1
-    elif (n > 1):
-         return (sum_series(n -1) + sum_series(n-2))
+    """Returns both fibonacci and lucas numbers"""
+    if (x == 0 and y == 1):
+        return(fibonacci(n))
+    if (x == 2 and y == 1):
+        return(lucas(n))
 
-print(sum_series(4))
-print(sum_series(5, x=2, y=1))
+print(sum_series(3, x=2, y=1))
+print(sum_series(3))
+
+
+
+
