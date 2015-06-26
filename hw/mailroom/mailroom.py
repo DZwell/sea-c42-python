22eb2e3b52d8ec5567b6044a815059b023ce9657
Daniel Zwelling <dzwellingmusic@gmail.com>
2015-06-25 17:45:13 -0700

    Added donor letter and donor list

--- hw/mailroom/mailroom.py
+++ hw/mailroom/mailroom.py
@@ -1,7 +1,7 @@
 import sys
 
 
-donor_list = [['Frank Zappa'], ['Geroge Benson'], ['Zorro'], ['Vito Corleone']]
+donor_list = ['Frank Zappa', 'Geroge Benson', 'Zorro', 'Vito Corleone']
 
 
 while True:
@@ -12,12 +12,23 @@ while True:
             "quit - Return to main menu")
         answer2 = input('> ')
         if (answer2 == 'list' or answer2 == 'List'):
-            print(donor_list[:])
+            print(donor_list[::])
             send_thanks()
         elif (answer2 == 'quit' or answer2 == 'Quit'):
             prompt_user()
         else:
-            print("")
+            if (answer2 in donor_list):
+                print("Enter a donation amount or 'quit:")
+                donation_amount = input('> ')
+                print("Dear %s,\n\n"
+                    "Thank you so much for your generous donatation of $%d. "
+                    "\nWe here at 'Dogs Are People Too' couldn't succed in "
+                    "advocating for human rights for dogs "
+                    "\nwith out people like you. With election season coming up, "
+                    "your money will go towards a new piece of legislation "
+                    "desinged to give dogs access to free public education.\n"
+                    "\nThank you again,\n\nDaniel Zwelling\n\nPresident, D.A.P.T\n\n "
+                    % (answer2, int(donation_amount)))
 
     def prompt_user():
         print("Welcome to Mailroom Madness\nChoose from the following:\n"
@@ -32,41 +43,4 @@ while True:
     prompt_user()
 
 
-
-
-
-
-    # if (user input == 'q'):
-    #     def Quit script():
-
-    # prompt = answer
-    #     if (user input == 'q'):
-    #         def Quit prompt():
-
-    #     if (prompt == send thankyou):
-    #         if (user asks to see list):
-    #             def show list of donor names, reprompt():
-
-    #         if (name typed in doesn't exist):
-    #             def add name to [],():
-
-    #         elif (name exists):
-    #             def prompt for donation amount():
-
-    #             if (donation is number):
-    #                 add amount to donation history
-    #                 print("Thank you %s for your donation of $ %d ")
-    #                 Back to original prompt
-    #             else:
-    #                 reprompt
-    #     else:
-    #         if (user input == 'q'):
-    #         def Quit prompt():
-
-    #         print list of donors sorted by total historical donation amount
-    #             list = [donor name, total donated,
-    #             number of donations, avg donation amount]
-
-    #             print in nice rows and columns
-    #             Back to original prompt
 
